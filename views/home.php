9eba1d85cac7be922c8596b22c923b9532dbdb7c
fabiuz <ccmaismais@yahoo.com>
2017-08-13 18:13:29 -0300

    Término da aula 41, tudo funcionando conforme o vídeo da aula.

--- views/home.php
+++ views/home.php
@@ -1,1 +1,70 @@
-<h5>Home</h5>
+<div class="db-row row1">
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+</div>
+
+<div class="db-row row2">
+    <div class="grid-2">
+       <div class="db-info">
+           <div class="db-info-title">
+               Aqui vai o título.
+           </div>
+           <div class="db-info-body">
+                Aqui vai o corpo.
+           </div>
+       </div>
+    </div>
+    <div class="grid-1">
+        <div class="db-info">
+            <div class="db-info-title">
+                Aqui vai o título.
+            </div>
+            <div class="db-info-body">
+                Aqui vai o corpo.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="db-row row2">
+    <div class="grid-2">
+        <div class="db-info">
+            <div class="db-info-title">
+                Aqui vai o título.
+            </div>
+            <div class="db-info-body">
+                Aqui vai o corpo.
+            </div>
+        </div>
+    </div>
+    <div class="grid-1">
+        <div class="db-info">
+            <div class="db-info-title">
+                Aqui vai o título.
+            </div>
+            <div class="db-info-body">
+                Aqui vai o corpo.
+            </div>
+        </div>
+    </div>
+</div>
+
+
